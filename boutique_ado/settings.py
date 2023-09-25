288e4e4eba1b1188f2a2a699830e4fb286de8084
Ethan Pitt <ShadowOfNyxx@proton.me>
2023-09-25 13:41:35 +0000

    Fix typo in settings.py

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -227,12 +227,12 @@ DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
 if 'DEVELOPMENT' in os.environ:
     # Log confirmation emails to console
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL='boutiqueado@example.com'
+    DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
-    EMAIL_HOST_PASS = os.environ.get("EMAIL_HOST_PASS")
+    EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASS")
     DEFAULT_FROM_EMAIL = os.environ.get("EMAIL_HOST_USER")
